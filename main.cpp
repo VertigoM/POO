7a3a429578aad1148b4af38435df7509d6581268
Alecu Mihai <mihai-marian.alecu@my.fmi.unibuc.ro>
2020-11-01 21:10:59 +0200

    -

--- main.cpp
+++ main.cpp
@@ -2,22 +2,15 @@
 #include "character.h"
 
 int main(){
-    /*Character character1("Gandalf", Elf(), new Fire_wizzard);
+    Character character1("Gandalf", Elf(), new Fire_wizzard);
     Character character2 = character1;
     character1._specialization->Fire_ball();
 
 
     std::printf("[DEBUG]%p\n", character1._specialization);
-    std::printf("[DEBUG]%p\n", character2._specialization);*/
+    std::printf("[DEBUG]%p\n", character2._specialization);
 
-    Character* character1 = new Character("Gandalf", Elf(), new Fire_wizzard);
-    Character* character2 = new Character;
+    character1._specialization->do_attack();
 
-    character2 = character1;
-
-    std::printf("[DEBUG]%p\n", character1 -> _specialization);
-    std::printf("[DEBUG]%p\n", character2 -> _specialization);
-
-    character1->_specialization->do_attack();
     return 0;
 }
